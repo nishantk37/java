091bb38b56c3cc336b65b3b05e9e69b047488c60
nishantk37 <73989433+nishantk37@users.noreply.github.com>
2020-11-22 11:41:17 +0530

    Create Practical-13.java

---
+++ Practical-13.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Enter two numbers: ");
+
+    double first = reader.nextDouble();
+    double second = reader.nextDouble();
+
+    System.out.print("Enter an operator (+, -, *, /): ");
+    char operator = reader.next().charAt(0);
+
+    double result;
+
+    switch (operator) {
+      case '+':
+        result = first + second;
+        break;
+
+      case '-':
+        result = first - second;
+        break;
+
+      case '*':
+        result = first * second;
+        break;
+
+      case '/':
+        result = first / second;
+        break;
+
+      default:
+        System.out.printf("Error! operator is not correct");
+        return;
+    }
+
+    System.out.println(first + " " + operator + " " + second + " = " + result);
+  }
+}
