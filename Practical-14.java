f8e096d28b6486be80fde46a302202a4687f3858
nishantk37 <73989433+nishantk37@users.noreply.github.com>
2020-11-22 11:45:11 +0530

    Create Practical-14.java

---
+++ Practical-14.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    
+    
+    String first = "Java";
+    String second = "Python";
+    String third = "JavaScript";
+    String fourth = "C++";
+    String fifth = "C";
+
+    
+    System.out.println(first);  
+    System.out.println(second);
+    System.out.println(third); 
+    System.out.println(fourth);
+    System.out.println(fifth);
+  }
+}
