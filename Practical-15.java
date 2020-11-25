af9ee57d4a40344fc36393e6021dc4990d8b2f20
nishantk37 <73989433+nishantk37@users.noreply.github.com>
2020-11-25 12:34:26 +0530

    Create Practical-15.java

---
+++ Practical-15.java
@@ -0,0 +1,13 @@
+public class Commandlineadd 
+{  
+public static void main(String args[])  
+{  
+int x,y,sum;  
+a=Integer.parseInt(args[0]);      
+b=Integer.parseInt(args[1]);  
+System.out.println("The first input number is :" +x);
+System.out.println("The second input number is :" +y);
+sum=x+y; 
+System.out.println("The sum of " + x + " and " + y +" is " +sum);  
+}  
+}
