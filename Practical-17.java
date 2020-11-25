0155f46ca561537402085fb1c0d6f24f0e294c86
nishantk37 <73989433+nishantk37@users.noreply.github.com>
2020-11-25 12:37:56 +0530

    Create Practical-17.java

---
+++ Practical-17.java
@@ -0,0 +1,17 @@
+public class Methodoverloading
+{
+  public static void sum(int x, int y)
+  {
+    System.out.println("sum is "+(x+y)) ;
+  }
+  public static void sum(float x, float y)
+  {
+    System.out.println("sum is "+(x+y));
+  }
+  public static void main (String[] args)
+  {
+    Methodoverloading cal = new Methodoverloading();
+    cal.sum (7,4;      
+    cal.sum (5.6f, 2.8f);
+
+}
